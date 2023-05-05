f02b1aed115b5a0c7c7afeea22c0ee20afcb280f
Edin2022 <114033441+Edin2022@users.noreply.github.com>
2023-05-05 13:48:15 -0600

    progando

---
+++ nah.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num1;
+    int num2;
+    int resultado;
+
+    cout<<"Ingrese el primer numero: " <<endl;
+    cin>>num1;
+
+    cout<<"Ingrese el segundo numero: " <<endl;
+    cin>>num2;
+
+    resultado = num1 + num2;
+
+    cout<<"El resultado de la suma es: " <<resultado <<endl;
+
+
+
+
+
+
+    return 0;
+}
